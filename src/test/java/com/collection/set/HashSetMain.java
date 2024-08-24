210f38de1b3b34586bffbf683fcbe1d0c38d8a49
dada <dadasahebkale.dk@gmail.com>
2024-08-24 08:43:23 +0530

    Updated

--- src/test/java/com/collection/set/HashSetMain.java
+++ src/test/java/com/collection/set/HashSetMain.java
@@ -1,5 +1,8 @@
 package com.collection.set;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class HashSetMain {
 
 	public static void main(String[] args) {
@@ -16,7 +19,26 @@ public class HashSetMain {
 		//if default size 16 is completed and more than 16 element added and another object
 		//will be created previous object copy into other element first object moved to GB
 		
-
+		HashSet set=new HashSet();
+		set.add(100);
+		set.add("Welcome");
+		set.add(16.4);
+		set.add('A');
+		set.add(100);
+		set.add(true);
+		set.add(null);
+		set.add("Hello");
+		System.out.println(set);
+		set.remove(null);
+		System.out.println(set);
+		System.out.println(set.contains("Hello"));
+		System.out.println(set.isEmpty());
+		
+		// using iterator
+		Iterator it=set.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
 	}
 
 }
